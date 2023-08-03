55ce5b65d5eebfb1390f94792fe0b79fd3753271
ebl3907 <23806861-ebl3907@users.noreply.replit.com>
2023-08-03 17:29:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+message = "Welcome to AI Cable"
+print(message)
+message = "Save money on cable length"
+print(message)
+name = input("Please tell us your company name: ")
+print(f"\nWelcome, {name}!")
+feet = float(input("How much cable would you like to purchase?: "))
+if feet>500:
+  total_cost=feet*0.50
+elif feet>250:
+  total_cost=feet*0.70
+elif feet>100:
+  total_cost=feet*0.80
+else:
+  total_cost=feet*0.87
+print("Total_cost of Fiber Cable : $", total_cost, "for",name, "to purchase",feet, "ft.")
